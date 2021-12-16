6d11c5175164464a38a4a67108c9eb0a1f0857df
Shauzen-Prog <80788112+Shauzen-Prog@users.noreply.github.com>
2021-12-16 19:20:27 -0300

    Inicial Commit

---
+++ Assets/Scripts/EditModeTest/Test1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace EditModeTest
+{
+    public class Test1
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Test1SimplePasses()
+        {
+            Assert.AreEqual(2 + 2, 4);
+            
+        }
+    }
+}
